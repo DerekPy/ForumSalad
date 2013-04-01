ebc1e749ddafca5289aeaa0450dbfa5c419f9076
Gravitation <gravi@tation.tk>
2013-04-01 19:38:58 +0200

    Some fragments

--- index.php
+++ index.php
@@ -1,2 +1,36 @@
+<<<<<<< HEAD
 <title>[Board Name]</title>
 <link rel="stylesheet" type="text/css" href="/style/[User's Style].css">
+=======
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>ForumSalad - New Forum</title>
+		<meta name="Description" content="A forum powered by ForumSalad.">
+		<meta name="Keywords" content="forum forums" />
+		<link rel="Stylesheet" href="css.css" />
+		<link rel="shortcut icon" href="favicon.ico" />
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+	</head>
+	<body>
+		<div id="topnav">
+			<div id="tbspacing">
+				<div id="mn">
+					MESSAGES <img src="notifications.png" />
+				</div>
+				<div id="u">
+					USER LOGOUT
+				</div>
+			</div>
+		</div>
+		<div id="wrapper">
+			<div id="header">
+				WELCOME TO MY FORUM!!!!!!!!!!!!!111
+			</div>
+			<div id="content">
+				omgomgomgomgomg
+			</div>
+		</div>
+	</body>
+</html>
+>>>>>>> Some broken fragments
